98d2aecd9945249d393ed2559c7e55665ff0adf8
Mouhamednd <mouhamednd7@gmail.com>
2024-03-28 00:40:45 -0400

    adding HomePage, ContactPage and router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 
 function App() {
   return (
     <div>
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
